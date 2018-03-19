fc49fb05a109e829c4a645917e1702d848442179
Niccolò Abate <sylathas@MacBook-Pro-di-Niccolo-2.local>
2018-03-19 23:18:17 +0100

    Piccoli cambiamenti
    Piccole modificazioni al javascript della homepage

--- grid.js
+++ grid.js
@@ -17,8 +17,11 @@ function openDiv() {
 
 function closeDiv() {
 	document.getElementById("Traveldiv").style.height = "100px";
-	document.getElementById("Traveldiv").style.zIndex = "0";
 	document.getElementById("Close").style.opacity = "0";
+}
+
+function zindex1(){
+	document.getElementById("Traveldiv").style.zIndex = "0";
 }
 
 function openDiv2() {
@@ -30,9 +33,12 @@ function openDiv2() {
 
 function closeDiv2() {
 	document.getElementById("Designdiv").style.height = "100px";
-	document.getElementById("Designdiv").style.zIndex = "0";
 	document.getElementById("Designdiv").style.top = "400px";
 	document.getElementById("Close1").style.opacity = "0";
+}
+
+function zindex2(){
+	document.getElementById("Designdiv").style.zIndex = "0";
 }
 
 function openDiv3() {
@@ -44,9 +50,12 @@ function openDiv3() {
 
 function closeDiv3() {
 	document.getElementById("Musicdiv").style.height = "100px";
-	document.getElementById("Musicdiv").style.zIndex = "0";
 	document.getElementById("Musicdiv").style.top = "500px";
 	document.getElementById("Close2").style.opacity = "0";
+}
+
+function zindex3(){
+	document.getElementById("Musicdiv").style.zIndex = "0";
 }
 
 function openDiv4() {
@@ -58,7 +67,10 @@ function openDiv4() {
 
 function closeDiv4() {
 	document.getElementById("Filmdiv").style.height = "100px";
-	document.getElementById("Filmdiv").style.zIndex = "0";
 	document.getElementById("Filmdiv").style.top = "600px";
 	document.getElementById("Close3").style.opacity = "0";
+}
+
+function zindex4(){
+	document.getElementById("Filmdiv").style.zIndex = "0";
 }
