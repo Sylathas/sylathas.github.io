f272c7e8b84cc601860dba982c122da07353906b
Niccolò Abate <sylathas@MacBook-Pro-di-Niccolo-2.local>
2018-05-09 20:28:31 +0200

    Cambio grafica
    -Tolto diverse scritte principali e cambiato grafica del titolo
    -tolto il menù
    -reso le varie barre quando cliccate larghe quanto lo schermo

--- js/grid.js
+++ js/grid.js
@@ -1,23 +1,15 @@
 // JavaScript Document
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginRight = "250px";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginRight = "0";
-}
-
 function openDiv() {
-	document.getElementById("Traveldiv").style.height = "400px";
+	document.getElementById("Traveldiv").style.height = "100vh";
 	document.getElementById("Traveldiv").style.zIndex = "1";
+	document.getElementById("Traveldiv").style.top = "0px";
 	document.getElementById("Close").style.opacity = "1";
 }
 
 function closeDiv() {
 	document.getElementById("Traveldiv").style.height = "100px";
 	document.getElementById("Close").style.opacity = "0";
+	document.getElementById("Traveldiv").style.top = "300px";
 }
 
 function zindex1(){
@@ -25,9 +17,9 @@ function zindex1(){
 }
 
 function openDiv2() {
-	document.getElementById("Designdiv").style.height = "400px";
+	document.getElementById("Designdiv").style.height = "100vh";
 	document.getElementById("Designdiv").style.zIndex = "1";
-	document.getElementById("Designdiv").style.top = "300px";
+	document.getElementById("Designdiv").style.top = "0px";
 	document.getElementById("Close1").style.opacity = "1";
 }
 
@@ -42,9 +34,9 @@ function zindex2(){
 }
 
 function openDiv3() {
-	document.getElementById("Musicdiv").style.height = "400px";
+	document.getElementById("Musicdiv").style.height = "100vh";
 	document.getElementById("Musicdiv").style.zIndex = "1";
-	document.getElementById("Musicdiv").style.top = "300px";
+	document.getElementById("Musicdiv").style.top = "0px";
 	document.getElementById("Close2").style.opacity = "1";
 }
 
@@ -59,9 +51,9 @@ function zindex3(){
 }
 
 function openDiv4() {
-	document.getElementById("Filmdiv").style.height = "400px";
+	document.getElementById("Filmdiv").style.height = "100vh";
 	document.getElementById("Filmdiv").style.zIndex = "1";
-	document.getElementById("Filmdiv").style.top = "300px";
+	document.getElementById("Filmdiv").style.top = "0px";
 	document.getElementById("Close3").style.opacity = "1";
 }
 
@@ -73,4 +65,8 @@ function closeDiv4() {
 
 function zindex4(){
 	document.getElementById("Filmdiv").style.zIndex = "0";
+}
+
+function Link(){
+	 location.href='index.html';
 }
