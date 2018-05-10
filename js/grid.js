701d59be0cb357a1aefefb9855c5d6ecf1b97c3e
Niccolò Abate <sylathas@MacBook-Pro-di-Niccolo-2.local>
2018-05-10 09:31:08 +0200

    Aggiunta sezione
    -Aggiunta sezione Europa in Travel

--- js/grid.js
+++ js/grid.js
@@ -1,15 +1,30 @@
 // JavaScript Document
 function openDiv() {
+	var x = document.getElementsByClassName("Europe");
+	var i;
 	document.getElementById("Traveldiv").style.height = "100vh";
 	document.getElementById("Traveldiv").style.zIndex = "1";
 	document.getElementById("Traveldiv").style.top = "0px";
 	document.getElementById("Close").style.opacity = "1";
+	for (i = 0; i < x.length; i++) {
+    	x[i].style.opacity = "0";
+		x[i].style.zIndex = "0";
+	}
 }
 
 function closeDiv() {
+	var x = document.getElementsByClassName("Europe");
+	var i;
 	document.getElementById("Traveldiv").style.height = "100px";
 	document.getElementById("Close").style.opacity = "0";
 	document.getElementById("Traveldiv").style.top = "300px";
+	document.getElementById("Travelp").style.fontSize = "60px";
+	document.getElementById("World").style.opacity = "0";
+	document.getElementById("Europe").style.opacity = "0";
+	for (i = 0; i < x.length; i++) {
+    	x[i].style.opacity = "0";
+		x[i].style.zIndex = "0";
+	}
 }
 
 function zindex1(){
@@ -68,5 +83,42 @@ function zindex4(){
 }
 
 function Link(){
-	 location.href='index.html';
+	location.href='index.html';
+}
+
+/*Travel*/
+
+function Image1(){
+	document.getElementById("Europe").style.opacity = "1";
+	document.getElementById("Europe").style.zIndex = "1";
+}
+
+function Image2(){
+	document.getElementById("World").style.opacity = "1";
+	document.getElementById("World").style.zIndex = "1";
+}
+
+/*Europa*/
+
+function ViaggiE(){
+	var x = document.getElementsByClassName("Europe");
+	var i;
+	document.getElementById("World").style.opacity = "0";
+	document.getElementById("Europe").style.opacity = "0";
+	for (i = 0; i < x.length; i++) {
+    	x[i].style.opacity = "1";
+		x[i].style.zIndex = "1";
+	}
+}
+
+function Back(){
+	var x = document.getElementsByClassName("Europe");
+	var i;
+	for (i = 0; i < x.length; i++) {
+    	x[i].style.opacity = "0";
+		x[i].style.zIndex = "0";
+	}
+	document.getElementById("World").style.opacity = "1";
+	document.getElementById("Europe").style.opacity = "1";
+	
 }
