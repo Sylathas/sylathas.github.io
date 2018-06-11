b6e6d85268dd486892c46caf7083a70d8e01e424
sylathas <abate.niccolo@gmail.com>
2018-06-11 12:33:59 +0200

    Fine musica
    -Fine della sezione musica (anche se con possibilità di miglioramento mettendo audio ad ogni band)
    -Cambio della sezione "Film" in "Interessi"
    -Messo testo sotto il buffer in modo da avvertire il visitatore del caricamento del sito

--- js/grid.js
+++ js/grid.js
@@ -81,6 +81,56 @@ function closeDiv3() {
 	document.getElementById("Musicdiv").style.height = "100px";
 	document.getElementById("Musicdiv").style.top = "500px";
 	document.getElementById("Close2").style.opacity = "0";
+	document.getElementById("PopPunk").style.opacity = "0";
+	document.getElementById("Pop").style.opacity = "0";
+	document.getElementById("PopPunk").style.zIndex = "0";
+	document.getElementById("Pop").style.zIndex = "0";
+	document.getElementById("Acoustic").style.opacity = "0";
+	document.getElementById("Metal").style.opacity = "0";
+	document.getElementById("Acoustic").style.zIndex = "0";
+	document.getElementById("Metal").style.zIndex = "0";
+	document.getElementById("Metalcore").style.opacity = "0";
+	document.getElementById("Rock").style.opacity = "0";
+	document.getElementById("Metalcore").style.zIndex = "0";
+	document.getElementById("Rock").style.zIndex = "0";
+	document.getElementById("Rap").style.opacity = "0";
+	document.getElementById("Rap").style.zIndex = "0";
+	var a = document.getElementsByClassName("PopPunk");
+	var b = document.getElementsByClassName("Pop");
+	var c = document.getElementsByClassName("Metal");
+	var d = document.getElementsByClassName("Metalcore");
+	var e = document.getElementsByClassName("Acoustic");
+	var f = document.getElementsByClassName("Rock");
+	var g = document.getElementsByClassName("Rap");
+	var i;
+	for (i = 0; i < a.length; i++) {
+    	a[i].style.opacity = "0";
+		a[i].style.zIndex = "0";
+	}
+	for (i = 0; i < b.length; i++) {
+    	b[i].style.opacity = "0";
+		b[i].style.zIndex = "0";
+	}
+	for (i = 0; i < c.length; i++) {
+    	c[i].style.opacity = "0";
+		c[i].style.zIndex = "0";
+	}
+	for (i = 0; i < d.length; i++) {
+    	d[i].style.opacity = "0";
+		d[i].style.zIndex = "0";
+	}
+	for (i = 0; i < e.length; i++) {
+    	e[i].style.opacity = "0";
+		e[i].style.zIndex = "0";
+	}
+	for (i = 0; i < f.length; i++) {
+    	f[i].style.opacity = "0";
+		f[i].style.zIndex = "0";
+	}
+	for (i = 0; i < g.length; i++) {
+    	g[i].style.opacity = "0";
+		g[i].style.zIndex = "0";
+	}
 }
 
 function zindex3(){
@@ -171,4 +221,201 @@ function Image3(){
 function Image4(){
 	document.getElementById("Personal").style.opacity = "1";
 	document.getElementById("Personal").style.zIndex = "1";
+}
+
+/*Music*/
+
+function Image5(){
+	document.getElementById("PopPunk").style.opacity = "1";
+	document.getElementById("PopPunk").style.zIndex = "1";
+}
+
+function Image6(){
+	document.getElementById("Pop").style.opacity = "1";
+	document.getElementById("Pop").style.zIndex = "1";
+}
+
+function Image7(){
+	document.getElementById("Metalcore").style.opacity = "1";
+	document.getElementById("Metalcore").style.zIndex = "1";
+}
+
+function Image8(){
+	document.getElementById("Metal").style.opacity = "1";
+	document.getElementById("Metal").style.zIndex = "1";
+}
+
+function Image9(){
+	document.getElementById("Acoustic").style.opacity = "1";
+	document.getElementById("Acoustic").style.zIndex = "1";
+}
+
+function Image10(){
+	document.getElementById("Rock").style.opacity = "1";
+	document.getElementById("Rock").style.zIndex = "1";
+}
+
+function Image11(){
+	document.getElementById("Rap").style.opacity = "1";
+	document.getElementById("Rap").style.zIndex = "1";
+}
+
+//Music
+
+function BackM(){
+	var a = document.getElementsByClassName("PopPunk");
+	var b = document.getElementsByClassName("Pop");
+	var c = document.getElementsByClassName("Metal");
+	var d = document.getElementsByClassName("Metalcore");
+	var e = document.getElementsByClassName("Acoustic");
+	var f = document.getElementsByClassName("Rock");
+	var g = document.getElementsByClassName("Rap");
+	var i;
+	for (i = 0; i < a.length; i++) {
+    	a[i].style.opacity = "0";
+		a[i].style.zIndex = "0";
+	}
+	for (i = 0; i < b.length; i++) {
+    	b[i].style.opacity = "0";
+		b[i].style.zIndex = "0";
+	}
+	for (i = 0; i < c.length; i++) {
+    	c[i].style.opacity = "0";
+		c[i].style.zIndex = "0";
+	}
+	for (i = 0; i < d.length; i++) {
+    	d[i].style.opacity = "0";
+		d[i].style.zIndex = "0";
+	}
+	for (i = 0; i < e.length; i++) {
+    	e[i].style.opacity = "0";
+		e[i].style.zIndex = "0";
+	}
+	for (i = 0; i < f.length; i++) {
+    	f[i].style.opacity = "0";
+		f[i].style.zIndex = "0";
+	}
+	for (i = 0; i < g.length; i++) {
+    	g[i].style.opacity = "0";
+		g[i].style.zIndex = "0";
+	}
+	document.getElementById("PopPunk").style.opacity = "1";
+	document.getElementById("Pop").style.opacity = "1";
+	document.getElementById("Metal").style.opacity = "1";
+	document.getElementById("Metalcore").style.opacity = "1";
+	document.getElementById("Acoustic").style.opacity = "1";
+	document.getElementById("Rock").style.opacity = "1";
+	document.getElementById("Rap").style.opacity = "1";	
+}
+
+function MusicPp(){
+	var a = document.getElementsByClassName("PopPunk");
+	document.getElementById("PopPunk").style.opacity = "0";
+	document.getElementById("Pop").style.opacity = "0";
+	document.getElementById("Metal").style.opacity = "0";
+	document.getElementById("Metalcore").style.opacity = "0";
+	document.getElementById("Acoustic").style.opacity = "0";
+	document.getElementById("Rock").style.opacity = "0";
+	document.getElementById("Rap").style.opacity = "0";
+	var i;
+	for (i = 0; i < a.length; i++) {
+    	a[i].style.opacity = "1";
+		a[i].style.zIndex = "1";
+	}
+}
+
+function MusicP(){
+	var a = document.getElementsByClassName("Pop");
+	document.getElementById("PopPunk").style.opacity = "0";
+	document.getElementById("Pop").style.opacity = "0";
+	document.getElementById("Metal").style.opacity = "0";
+	document.getElementById("Metalcore").style.opacity = "0";
+	document.getElementById("Acoustic").style.opacity = "0";
+	document.getElementById("Rock").style.opacity = "0";
+	document.getElementById("Rap").style.opacity = "0";
+	var i;
+	for (i = 0; i < a.length; i++) {
+    	a[i].style.opacity = "1";
+		a[i].style.zIndex = "1";
+	}
+}
+
+function MusicM(){
+	var a = document.getElementsByClassName("Metal");
+	document.getElementById("PopPunk").style.opacity = "0";
+	document.getElementById("Pop").style.opacity = "0";
+	document.getElementById("Metal").style.opacity = "0";
+	document.getElementById("Metalcore").style.opacity = "0";
+	document.getElementById("Acoustic").style.opacity = "0";
+	document.getElementById("Rock").style.opacity = "0";
+	document.getElementById("Rap").style.opacity = "0";
+	var i;
+	for (i = 0; i < a.length; i++) {
+    	a[i].style.opacity = "1";
+		a[i].style.zIndex = "1";
+	}
+}
+
+function MusicMc(){
+	var a = document.getElementsByClassName("Metalcore");
+	document.getElementById("PopPunk").style.opacity = "0";
+	document.getElementById("Pop").style.opacity = "0";
+	document.getElementById("Metal").style.opacity = "0";
+	document.getElementById("Metalcore").style.opacity = "0";
+	document.getElementById("Acoustic").style.opacity = "0";
+	document.getElementById("Rock").style.opacity = "0";
+	document.getElementById("Rap").style.opacity = "0";
+	var i;
+	for (i = 0; i < a.length; i++) {
+    	a[i].style.opacity = "1";
+		a[i].style.zIndex = "1";
+	}
+}
+
+function MusicA(){
+	var a = document.getElementsByClassName("Acoustic");
+	document.getElementById("PopPunk").style.opacity = "0";
+	document.getElementById("Pop").style.opacity = "0";
+	document.getElementById("Metal").style.opacity = "0";
+	document.getElementById("Metalcore").style.opacity = "0";
+	document.getElementById("Acoustic").style.opacity = "0";
+	document.getElementById("Rock").style.opacity = "0";
+	document.getElementById("Rap").style.opacity = "0";
+	var i;
+	for (i = 0; i < a.length; i++) {
+    	a[i].style.opacity = "1";
+		a[i].style.zIndex = "1";
+	}
+}
+
+function MusicR(){
+	var a = document.getElementsByClassName("Rock");
+	document.getElementById("PopPunk").style.opacity = "0";
+	document.getElementById("Pop").style.opacity = "0";
+	document.getElementById("Metal").style.opacity = "0";
+	document.getElementById("Metalcore").style.opacity = "0";
+	document.getElementById("Acoustic").style.opacity = "0";
+	document.getElementById("Rock").style.opacity = "0";
+	document.getElementById("Rap").style.opacity = "0";
+	var i;
+	for (i = 0; i < a.length; i++) {
+    	a[i].style.opacity = "1";
+		a[i].style.zIndex = "1";
+	}
+}
+
+function MusicRa(){
+	var a = document.getElementsByClassName("Rap");
+	document.getElementById("PopPunk").style.opacity = "0";
+	document.getElementById("Pop").style.opacity = "0";
+	document.getElementById("Metal").style.opacity = "0";
+	document.getElementById("Metalcore").style.opacity = "0";
+	document.getElementById("Acoustic").style.opacity = "0";
+	document.getElementById("Rock").style.opacity = "0";
+	document.getElementById("Rap").style.opacity = "0";
+	var i;
+	for (i = 0; i < a.length; i++) {
+    	a[i].style.opacity = "1";
+		a[i].style.zIndex = "1";
+	}
 }
